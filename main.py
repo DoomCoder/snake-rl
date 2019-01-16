8c3381d07b3da92d1e51a5cc2e734ac17199b70b
Erhaven <laurence.tw@gmail.com>
2019-01-16 11:48:34 +0100

    Add reporter class, use it in main

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 import gym
-
+from reporter import Reporter
 from simpleDQN import SimpleDQNAgent
 from convDQN import ConvDQNAgent
 
 
-N_EPISODES = 10**7
+MAX_EPISODES = 10 ** 7
+STATS_N_EPISODES = 100  # stats calculated on this many last episodes
+STATS_FREQ = 50  # print stats every STATS_FREQ number of episodes
 
 if __name__ == "__main__":
     env = gym.make('snake-v0')
     action_size = env.action_space.n
-    # agent = SimpleDQNAgent(env.observation_space.shape, action_size)
-    agent = ConvDQNAgent(env.observation_space.shape, action_size)
+    agent = SimpleDQNAgent(env.observation_space.shape, action_size)
+    # agent = ConvDQNAgent(env.observation_space.shape, action_size)
     # agent.load("./SNEK-dqn.h5")
     done = False
-    batch_size = 128
-    i = 0
+    batch_size = 64
+    reporter = Reporter(STATS_N_EPISODES, STATS_FREQ, MAX_EPISODES)
 
-    for e in range(N_EPISODES):
+    for e in range(MAX_EPISODES):
         state = env.reset()
         done = False
         # env.renderer.close_window()
@@ -38,12 +40,14 @@ if __name__ == "__main__":
             agent.remember(state, action, reward, next_state, done)
             state = next_state
             steps += 1
-            i += 1
             if done:
-                print("episode: {}/{}, steps: {}, score: {}, e: {:.2}"
-                      .format(e, N_EPISODES, steps, reward_sum, agent.epsilon))
+                reporter.remember(steps, len(env.game.snake.body), reward_sum)
+                if reporter.wants_to_report():
+                    print(reporter.get_report_str())
                 break
-            if len(agent.memory) > batch_size and (i % batch_size == 0):
+
+            if len(agent.memory) > batch_size:
                 agent.replay(batch_size)
+
         if e % 100 == 0:
             agent.save("./SNEK-dqn.h5")
---
+++ reporter.py
@@ -0,0 +1,30 @@
+from collections import deque
+import numpy as np
+
+
+class Reporter:
+    def __init__(self, stats_n_episodes, report_freq, max_episodes):
+        self.stats_n_episodes = stats_n_episodes
+        self._memory = deque([], stats_n_episodes)
+        self.report_freq = report_freq
+        self.n_episodes = 0
+        self.max_episodes = max_episodes
+
+    def remember(self, n_steps, snake_len, reward):
+        self._memory.append((n_steps, snake_len, reward))
+        self.n_episodes += 1
+
+    def get_report_str(self):
+        arr = np.array(self._memory)
+        means = np.mean(arr, axis=0)
+        report_format = 'Episodes {}/{} | avg steps: {}, avg len: {}, ' \
+                        'avg reward: {} (last {} games)'
+        report_data = (
+            self.n_episodes, self.max_episodes, means[0], means[1],
+            means[2], self.report_freq
+        )
+
+        return report_format.format(*report_data)
+
+    def wants_to_report(self):
+        return self.n_episodes % self.report_freq == 0
