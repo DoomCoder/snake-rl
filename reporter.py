3159143f031f0006d64196f645452013536db24d
Stanisław Giziński <s.gizinski84@gmail.com>
2019-01-20 23:57:02 +0100

    Update reporter with logging

--- reporter.py
+++ reporter.py
@@ -1,14 +1,26 @@
 from collections import deque
 import numpy as np
+import logging
 
 
 class Reporter:
-    def __init__(self, stats_n_episodes, max_episodes):
+    def __init__(self, stats_n_episodes, max_episodes, log_to_file=True):
         self.stats_n_episodes = stats_n_episodes
         self._memory = deque([], stats_n_episodes)
         self.n_episodes = 0
         self.max_episodes = max_episodes
-
+        if log_to_file:
+            self.lossLogger=self._init_logger()
+        
+    def _init_logger(self):
+        reportLogger=logging.getLogger("report")
+        reportLogger.setLevel(logging.DEBUG)
+        handler = logging.FileHandler("report.log")
+        handler.setLevel(logging.DEBUG)
+        reportLogger.addHandler(handler)
+        
+        return reportLogger
+    
     def remember(self, n_steps, snake_len, reward, epsilon):
         self._memory.append((n_steps, snake_len, reward, epsilon))
         self.n_episodes += 1
@@ -22,8 +34,11 @@ class Reporter:
             self.n_episodes, self.max_episodes, means[3], means[0], means[1],
             means[2], self.stats_n_episodes
         )
-
-        return report_format.format(*report_data)
+        formated_report=report_format.format(*report_data)
+        if self.lossLoger not None:
+            lossLoger.info(formated_report)
+        
+        return formated_report
 
     def wants_to_report(self):
         return self.n_episodes % self.stats_n_episodes == 0
