28a3b8ffb4a99b0b354ad7a747e3405194e52a0a
Stanisław Giziński <s.gizinski84@gmail.com>
2019-01-20 15:19:14 +0100

    Exiting from play view

--- play.py
+++ play.py
@@ -20,19 +20,20 @@ def watch_agent(agent: DQNAgent):
     env.__init__(human_mode=True)
     observation = env.reset()
     renderer=Renderer(env.game)
-    done = False
-    steps = 0
-    agent.epsilon = 0
-    state = agent.get_last_observations(observation)
-    while not done:
-        time.sleep(0.1)
-        renderer.render_frame()
-        action = agent.act(state)
-        next_observation, _, done, _ = env.step(action)
-        state = agent.get_last_observations(next_observation)
-        steps += 1
-
-    renderer.close_window()
+    try:
+        done = False
+        steps = 0
+        agent.epsilon = 0
+        state = agent.get_last_observations(observation)
+        while not done:
+            time.sleep(0.1)
+            renderer.render_frame()
+            action = agent.act(state)
+            next_observation, _, done, _ = env.step(action)
+            state = agent.get_last_observations(next_observation)
+            steps += 1
+    finally:
+        renderer.close_window()
     print(f"Snake lenght: {len(env.game.snake.body)}")
     print(f"Simulation ended after {steps} steps.")
     
