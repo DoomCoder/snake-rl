c1c0266d3f403d6453159a65557ad88411f2c677
pksiazek <piotr.ksiazek96@gmail.com>
2019-04-30 14:37:38 +0200

    Cleanup

--- convAC.py
+++ convAC.py
@@ -6,7 +6,6 @@ from reporter import Reporter
 import snake_logger
 from collections import deque
 from torchNet import *
-from torch.distributions import Categorical, OneHotCategorical
 qLogger=snake_logger.QLogger()
 
 class LoopKiller():
@@ -24,9 +23,8 @@ class LoopKiller():
         hashable_repr = str(state)
         return hashable_repr in self.memory
 
-class ConvDDPGAgent(DQNAgent):
+class ConvACAgent(DQNAgent):
     def __init__(self, state_shape, action_size, num_last_observations, loss_logging=True, rho=0.9):
-        # tf.enable_eager_execution()
         self.state_shape = state_shape
         self.action_size = action_size
         self.num_last_observations = num_last_observations
@@ -53,8 +51,6 @@ class ConvDDPGAgent(DQNAgent):
         self.policy_model.cuda()
         self.target_policy_model.cuda()
         self.cuda = torch.device('cuda')
-        # if loss_logging:
-        #     self.model.train_on_batch = snake_logger.loss_logger_decorator(self.model.train_on_batch)
 
     def remember(self, state, action, reward, next_state, done):
         state = self.reshape(state)
@@ -69,17 +65,8 @@ class ConvDDPGAgent(DQNAgent):
             return a, onehot_action
 
         act_values = self.policy_model(torch.from_numpy(np.expand_dims(state, 0)).float().to(self.cuda)).cpu()
-        # act_values = F.softmax(act_values, dim=1).cpu()
-        # act_values = self.q_model(torch.from_numpy(np.expand_dims(state, 0)).float().to(self.cuda)).cpu()
-        pr = np.random.rand()
-        # if pr <= 1e-2:
-        #     print(act_values)
-
         act_values = act_values[0].detach().numpy()
         a = np.random.choice(self.action_size, p=act_values)
-        # a = np.argmax(act_values)
-        # if pr <= 1e-2:
-        #     print(a)
 
         return a, act_values  # returns action
 
@@ -127,9 +114,6 @@ class ConvDDPGAgent(DQNAgent):
 
         q_table[np.arange(actions_arr_np.shape[0]), actions_arr_np] = updated_qs_for_taken_actions
         qs = self.q_model(states_arr)
-        pr = np.random.rand()
-        # if pr <= 1e-3:
-        #     print(np.linalg.norm(qs.detach().cpu().numpy()-q_table, ord=2))
 
         q_loss = 0.5*q_loss_fn(qs, torch.from_numpy(q_table).float().to(self.cuda))
         self.q_optimizer.zero_grad()
@@ -139,6 +123,7 @@ class ConvDDPGAgent(DQNAgent):
         policy_loss = pi_loss_fn(self.q_model, states_arr, actions)
         self.policy_optimizer.zero_grad()
         policy_loss.backward()
+        print(policy_loss)
         self.policy_optimizer.step()
 
     def train(self, env, batch_size, n_episodes, exploration_phase_size, report_freq, save_freq, models_dir):
@@ -163,13 +148,11 @@ class ConvDDPGAgent(DQNAgent):
                 # rewards can be changed here
                 if done:
                     reward = -1
-                    # reward = -1 * len(env.game.snake.body)
                 elif has_eaten:
                     reward = 1
                     # reward = len(env.game.snake.body)
                 elif loopKiller.seen(new_observation):
                     reward = -1
-                    # print("I helped!")
                     done = True
                 else:
                     reward = 0
@@ -199,23 +182,21 @@ class ConvDDPGAgent(DQNAgent):
                 self.epsilon -= self.epsilon_decay
 
             if e % save_freq == 0:
-                # model_path = os.path.join(models_dir, f'SNEK-ddpg-{e}-episodes.h5')
-                self.save(models_dir, e)
+                model_path = os.path.join(models_dir, f'SNEK-AC-{e}-episodes.h5')
+                self.save(model_path)
 
         self.save(models_dir, n_episodes)
 
-    def load(self, dir, e, nameQ=None, nameP=None):
-        if nameQ is None:
-            nameQ = f'SNEK-pg-3-Q-{e}-episodes.h5'
-        if nameP is None:
-            nameP = f'SNEK-pg-3-P-{e}-episodes.h5'
-
+    def load(self, dir):
+        nameQ = dir[:-3]+"-Q"+dir[-3:]
+        nameP = dir[:-3]+"-P"+dir[-3:]
         self.q_model.load_state_dict(torch.load(os.path.join(dir, nameQ)))
         # self.q_model.eval()
-        # model.eval()?
         self.policy_model.load_state_dict(torch.load(os.path.join(dir, nameP)))
         # self.policy_model.eval()
 
-    def save(self, dir, e):
-        torch.save(self.q_model.state_dict(), os.path.join(dir, f'SNEK-pg-2-Q-{e}-episodes.h5'))
-        torch.save(self.policy_model.state_dict(), os.path.join(dir, f'SNEK-pg-2-P-{e}-episodes.h5'))
+    def save(self, dir):
+        nameQ = dir[:-3]+"-Q"+dir[-3:]
+        nameP = dir[:-3]+"-P"+dir[-3:]
+        torch.save(self.q_model.state_dict(), nameQ)
+        torch.save(self.policy_model.state_dict(), nameP)
--- test_agent_play.py
+++ test_agent_play.py
@@ -1,7 +1,6 @@
 import gym
 import play
-# from convDQN import ConvDQNAgent
-from convDDPG import ConvDDPGAgent
+from convAC import ConvACAgent
 import sys
 import os
 
@@ -9,11 +8,8 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '4'
 sys.path.append('../snake_gym')
 
 env = gym.make('snake-v0')
-# agent = ConvDQNAgent(env.observation_space.shape, env.action_space.n, 3)
-agent = ConvDDPGAgent(env.observation_space.shape, env.action_space.n, 4,)
-# agent.load("./models/SNEK-dqnt-195000-episodes.h5")
+agent = ConvACAgent(env.observation_space.shape, env.action_space.n, 4, )
 agent.load("./models/", 20000, nameQ='SNEK-pg-2-Q-99000-episodes.h5', nameP='SNEK-pg-2-P-99000-episodes.h5')
-# agent.load("./models/...")
 
 while True:
     play.watch_agent(agent)
